ae11ff93a0a1374ee98b2a7a98de8a910e826422
Manan Kumar Garg <62146744+MananKGarg@users.noreply.github.com>
2021-06-16 02:31:18 +0530

    Create Trailing Zeros.cpp

---
+++ CSES Problem Set/Trailing Zeros.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ 
+int main(){
+    ll n;
+    cin >> n;
+    ll  ans = 0;
+    while(n > 0){
+        ans = ans + (n/5);
+        n = n / 5;
+    }
+    cout << ans;
