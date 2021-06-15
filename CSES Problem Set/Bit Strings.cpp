a4530e91fc50fe0876e72e15f309bffb653614e2
Manan Kumar Garg <62146744+MananKGarg@users.noreply.github.com>
2021-06-16 02:30:38 +0530

    Create Bit Strings.cpp

---
+++ CSES Problem Set/Bit Strings.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ 
+int main(){
+    ll n;
+    cin >> n;
+    ll mod = 1000000007;
+    ll ans = 1;
+    for(int i = 1; i < n+1; i++){
+        ans = (2 * (ans % mod)) % mod;
+    }
+    ans = ans % mod;
+    cout << ans;
+ }
