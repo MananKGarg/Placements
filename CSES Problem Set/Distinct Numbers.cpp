08f10067002000986fc620030ff4cff216231199
Manan Kumar Garg <62146744+MananKGarg@users.noreply.github.com>
2021-06-18 01:28:08 +0530

    Create Distinct Numbers.cpp

---
+++ CSES Problem Set/Distinct Numbers.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+    ll n;
+    cin >> n;
+    vector <ll> v;
+    for(ll i = 0; i < n; i++){
+        ll entry;
+        cin >> entry;
+        v.push_back(entry);
+    }
+    set <ll> a;
+    for(ll x:v){
+        a.insert(x);
+    }
+    cout << a.size();
+}
