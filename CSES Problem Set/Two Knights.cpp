50e40ef6c0c69d1f6eb63c7fd242537aa4e4d246
Manan Kumar Garg <62146744+MananKGarg@users.noreply.github.com>
2021-06-28 02:59:17 +0530

    Update Two Knights.cpp

--- CSES Problem Set/Two Knights.cpp
+++ CSES Problem Set/Two Knights.cpp
@@ -1,3 +1,10 @@
+/*
+Total number of ways to place two knights = ((n*n) * ((n*n) - 1)) / 2. 
+Two knights can attack each other only if they are on a 2 * 3 board. Number of 2*3 boards on an n*n grid = (n-1) * (n-2). Same for 3*2. For each board, we have two 
+possible ways of placing knights. Thus total placings in which knights can attack each other are 4 * (n-1) * (n-2).
+*/
+
+
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
