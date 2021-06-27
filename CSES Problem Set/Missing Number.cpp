54913460f21276b7464fc8c1458d317784c6774c
Manan Kumar Garg <62146744+MananKGarg@users.noreply.github.com>
2021-06-28 03:07:29 +0530

    Update Missing Number.cpp

--- CSES Problem Set/Missing Number.cpp
+++ CSES Problem Set/Missing Number.cpp
@@ -1,3 +1,15 @@
+/*
+Make an array with numbers from 1 to n. take XOR of all the elements of given and new array. Take XOR of both results.
+1. num ^ 0 = num
+2. num ^ num = 0
+
+example - n = 4, arr = {1,2,3}
+make an array b = {1,2,3,4}
+take XOR of all elements in arr and b.
+
+ans = 1^2^3^1^2^3^4 = 4
+*/
+
 #include <bits/stdc++.h>
  
 using namespace std;
